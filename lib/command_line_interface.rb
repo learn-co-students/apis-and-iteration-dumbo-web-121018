799fcf4c78bfc06726506a7b0b5997918a68447d
kenneth-yu <kyu22@buffalo.edu>
2018-12-11 14:47:34 -0500

    added bonus

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,6 +1,6 @@
 def welcome
   # puts out a welcome message here!
-  puts "Welcome to the Starwars API"
+  puts "Welcome to the Starwars API!"
 end
 
 def get_character_from_user
@@ -9,4 +9,19 @@ def get_character_from_user
   character_name.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
 end
+
+def new_query
+	puts "Would you like to make a new query?"
+	user_input = gets.chomp.downcase
+	if user_input != "yes" && user_input != "no"
+		puts "Invalid Entry. Please type 'Yes or No'"
+		user_input = gets.chomp.downcase
+	elsif user_input == "yes"
+		character = get_character_from_user
+		show_character_movies(character)
+		new_query
+	elsif user_input == "no"
+		exit
+	end
+end 
 
